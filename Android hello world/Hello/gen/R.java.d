d76dbd03f24811e39c6a4f5676d86460a2a05744
shyzkanza <shyzkanza@gmail.com>
2012-01-27 20:03:18 +0000

    Je supprime se que j'ai fait

--- Android hello world/Hello/gen/R.java.d
+++
@@ -1,4 +0,0 @@
-C:\Users\Shyzkanza\Desktop\projet Hello world\pokergame-l3\Android hello world\Hello\gen\projet\hello\R.java \
- : C:\Users\Shyzkanza\Desktop\projet Hello world\pokergame-l3\Android hello world\Hello\res\layout\main.xml \
-C:\Users\Shyzkanza\Desktop\projet Hello world\pokergame-l3\Android hello world\Hello\res\values\strings.xml \
-C:\Users\Shyzkanza\Desktop\projet Hello world\pokergame-l3\Android hello world\Hello\AndroidManifest.xml \
