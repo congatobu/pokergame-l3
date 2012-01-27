db7c015e9f7f26f001180f040045caca65592c1d
shyzkanza@gmail.com <shyzkanza@gmail.com@42773be0-cb6b-67bc-c68f-7b7c866795fe>
2012-01-27 20:03:18 +0000

    Je supprime se que j'ai fait

--- trunk/Android hello world/Hello/bin/classes.dex.d
+++
@@ -1,6 +0,0 @@
-C:\Users\Shyzkanza\Desktop\projet Hello world\pokergame-l3\Android hello world\Hello\bin\classes.dex : \
-C:\Users\Shyzkanza\Desktop\projet Hello world\pokergame-l3\Android hello world\Hello\bin\classes\projet\hello\HelloWorld.class \
-C:\Users\Shyzkanza\Desktop\projet Hello world\pokergame-l3\Android hello world\Hello\bin\classes\projet\hello\R$attr.class \
-C:\Users\Shyzkanza\Desktop\projet Hello world\pokergame-l3\Android hello world\Hello\bin\classes\projet\hello\R$layout.class \
-C:\Users\Shyzkanza\Desktop\projet Hello world\pokergame-l3\Android hello world\Hello\bin\classes\projet\hello\R$string.class \
-C:\Users\Shyzkanza\Desktop\projet Hello world\pokergame-l3\Android hello world\Hello\bin\classes\projet\hello\R.class \
