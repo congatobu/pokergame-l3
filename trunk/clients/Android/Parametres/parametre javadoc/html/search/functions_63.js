c11c10bfd0007c6692178f741d02a3612636c337
Ben.Maurin@gmail.com <Ben.Maurin@gmail.com@42773be0-cb6b-67bc-c68f-7b7c866795fe>
2012-02-04 14:12:15 +0000

    modification place android

---
+++ trunk/clients/Android/Parametres/parametre javadoc/html/search/functions_63.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['colorchanged',['colorChanged',['../interfaceprojet_1_1parametre_1_1_color_picker_dialog_1_1_on_color_changed_listener.html#ade54cbe03bdb65adee87212521c91952',1,'projet::parametre::ColorPickerDialog::OnColorChangedListener']]],
+  ['colorpickerdialog',['ColorPickerDialog',['../classprojet_1_1parametre_1_1_color_picker_dialog.html#a245b94f54b0851373181f4d6e5a59cca',1,'projet::parametre::ColorPickerDialog']]],
+  ['colorpickerview',['ColorPickerView',['../classprojet_1_1parametre_1_1_color_picker_dialog_1_1_color_picker_view.html#a463b52f34b76c3b36269e726959cf19f',1,'projet::parametre::ColorPickerDialog::ColorPickerView']]]
+];
---
+++ trunk/clients/Android/Parametres/parametre javadoc/html/search/variables_63.js
@@ -0,0 +1,8 @@
+var searchData=
+[
+  ['center_5fradius',['CENTER_RADIUS',['../classprojet_1_1parametre_1_1_color_picker_dialog_1_1_color_picker_view.html#a5a990891b42ed311376a50b79d97a85d',1,'projet::parametre::ColorPickerDialog::ColorPickerView']]],
+  ['center_5fx',['CENTER_X',['../classprojet_1_1parametre_1_1_color_picker_dialog_1_1_color_picker_view.html#a6fe25d67aa4045de3cbf191fe5c60c16',1,'projet::parametre::ColorPickerDialog::ColorPickerView']]],
+  ['center_5fy',['CENTER_Y',['../classprojet_1_1parametre_1_1_color_picker_dialog_1_1_color_picker_view.html#a32203d3a5e0acf91af93fda0595f51a4',1,'projet::parametre::ColorPickerDialog::ColorPickerView']]],
+  ['color',['color',['../classprojet_1_1parametre_1_1_r_1_1drawable.html#a9b1d535e7479ce6d0e8eff375bcbf0ee',1,'projet::parametre::R::drawable']]],
+  ['cpd',['cpd',['../classprojet_1_1parametre_1_1parametre.html#a8c669f79621e8774e6ba38b74c731a8e',1,'projet::parametre::parametre']]]
+];
