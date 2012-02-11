759ecab2da9c3e64fbed3fcaf0cdc2cd98798a24
Ben.Maurin@gmail.com <Ben.Maurin@gmail.com@42773be0-cb6b-67bc-c68f-7b7c866795fe>
2012-02-11 18:19:53 +0000

    Ajout fonction server liste parties en cours

--- trunk/serveur/PokerServer_début/PokerPartie.java
+++ trunk/serveur/PokerServer_début/PokerPartie.java
@@ -33,6 +33,7 @@ public class PokerPartie {
 	public String getEtat(){ return clientList.size()+"/"+maxPlayers+" joueurs dans la partie";}
 	public void setMaxPlayers(int m){this.maxPlayers=m;}
 	public int getMaxPlayers(){return this.maxPlayers;}
+	public int getNbJ(){return clientList.size();}
 	public String getNom(){return this.nomP;}
 	
 	/*
