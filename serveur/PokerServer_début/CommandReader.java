dcc3c899cdff9ca95f34efa0fd0a4e652b91cd65
Ben.Maurin <Ben.Maurin@gmail.com>
2012-02-13 18:03:49 +0000

    Rajout commentaires classes (javadoc) tous fichiers serveur DONT JEU HEIN STEVE

--- serveur/PokerServer_début/CommandReader.java
+++ serveur/PokerServer_début/CommandReader.java
@@ -1,65 +1,75 @@
-import java.net.*;
-import java.io.*;
-import java.util.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 
 
-/*
-Classe du lecteur de commandes
-*/
+/**
+ * Classe pour lire les commandes
+ * @author Ben maurin
+ */
 class CommandReader extends Thread{
-	BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-	PrintWriter screenOut = new PrintWriter(System.out, true);
-	String command;
-	
-	public CommandReader(){
-		super("CommandReader");
-	}
+    BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+    PrintWriter screenOut = new PrintWriter(System.out, true);
+    String command;
 
-	public void run(){
-		boolean valid;
-		try{
-			while ((command = in.readLine()) != null){
-				valid = false;
-				command = command.trim().toUpperCase();
-				if (command.compareTo("QUIT")==0){
-					valid = true;
-					PokerClientThread foo;
-					for (int i=0;i<PokerServer.clientList.size();++i){
-						foo= (PokerClientThread) PokerServer.clientList.get(i);
-						foo.deco();
-						}												
-					PokerServer.serverSocket.close();
-					System.exit(0);
-				}
-				if (command.compareTo("LIST")==0){
-					valid = true;
-					screenOut.println("");
-					PokerServer.listClients();
-					screenOut.println("");
-				}
-				if (command.compareTo("LISTP")==0){
-					valid = true;
-					screenOut.println("");
-					PokerServer.listParties();
-					screenOut.println("");
-				}
-				if (command.compareTo("HELP")==0){
-					valid = true;
-					screenOut.println("\n LIST  - liste des clients connectes");
-					screenOut.println(" LISTP - liste des parties");
-					screenOut.println(" HELP  - liste des commandes");
-					screenOut.println(" QUIT  - deconnecte les clients et quitte le serveur \n");
-				}
-				if (!valid){
-					screenOut.println("\nError: tapez 'HELP' pour voir les commandes supportees\n");
-				}
-				screenOut.print(">");
-				screenOut.flush();
-			}
-		}
-		catch (IOException e){
-			e.printStackTrace();
-		}
-		
-	}
+    /**
+     * Constructeur
+     */
+    public CommandReader(){
+        super("CommandReader");
+    }
+
+    /**
+     * Fonction qui lit les commandes
+     */
+    @Override
+    public void run(){
+        boolean valid;
+        try{
+            while ((command = in.readLine()) != null){
+                valid = false;
+                command = command.trim().toUpperCase();
+                if (command.compareTo("QUIT")==0){
+                    valid = true;
+                    PokerClientThread foo;
+                    for (int i=0;i<PokerServer.clientList.size();++i){
+                        foo= (PokerClientThread) PokerServer.clientList.get(i);
+                        foo.deco();
+                    }												
+                    PokerServer.serverSocket.close();
+                    System.exit(0);
+                }
+                if (command.compareTo("LIST")==0){
+                    valid = true;
+                    screenOut.println("");
+                    PokerServer.listClients();
+                    screenOut.println("");
+                }
+                if (command.compareTo("LISTP")==0){
+                    valid = true;
+                    screenOut.println("");
+                    PokerServer.listParties();
+                    screenOut.println("");
+                }
+                if (command.compareTo("HELP")==0){
+                    valid = true;
+                    screenOut.println("\n LIST  - liste des clients connectes");
+                    screenOut.println(" LISTP - liste des parties");
+                    screenOut.println(" HELP  - liste des commandes");
+                    screenOut.println(" QUIT  - deconnecte les clients et quitte le serveur \n");
+                }
+                if (!valid){
+                    screenOut.println("\nError: tapez 'HELP' pour voir les commandes supportees\n");
+                }
+                screenOut.print(">");
+                screenOut.flush();
+            }
+        }
+        catch (IOException e){
+        e.printStackTrace();
+        }
+
+    }
 };
--- serveur/PokerServer_début/PokerPartie.java
+++ serveur/PokerServer_début/PokerPartie.java
@@ -1,94 +1,145 @@
 
-import java.net.*;
-import java.io.*;
-import java.util.*;
+
+import java.io.PrintWriter;
+import java.util.Vector;
 import java.util.concurrent.Semaphore;
 
-/* -----------------------------------------------------------------
-Classe du serveur qui va gérer les parties
-----------------------------------------------------------------
-*/
+
+/**
+ * Classe du serveur qui va gerer les parties
+ * @author ben
+ */
 public class PokerPartie {
 		
-	private Vector clientList= new Vector();
-	private PrintWriter screenOut = new PrintWriter(System.out, true);
-	private int enCours = 0;
-	private int maxPlayers = 8;
-	private Semaphore available = new Semaphore(1, true);
-	private String nomP = "";
-	
-	/*
-	Constructeur
-	*/
-	public PokerPartie(String nom,int max){
-		this.nomP = nom;
-		this.maxPlayers= max;
-	}
-	
-	
-	/*
-	accesseurs
-	*/
-	public Vector getClientList(){return clientList;}
-	public String getEtat(){ return clientList.size()+"/"+maxPlayers+" joueurs dans la partie";}
-	public void setMaxPlayers(int m){this.maxPlayers=m;}
-	public int getMaxPlayers(){return this.maxPlayers;}
-	public int getNbJ(){return clientList.size();}
-	public String getNom(){return this.nomP;}
-	
-	/*
-	Fonction pour envoyer un message à tous les clients de la partie
-	*/
-	public void broadcastClientsPartie(String m)	{
-		int i;
-		PokerClientThread foo;
-		for (i=0;i<clientList.size();++i){
-			foo = (PokerClientThread)clientList.get(i);
-			foo.send(m);
-										} 	
-											}
-	/*
-	Fonction pour enlever un client de la partie
-	*/
-	public void deleteClient(PokerClientThread deadClient){
-		try{
-		available.acquire();
-		int num;
-		num=clientList.indexOf(deadClient);
-		if(num!=-1){
-		clientList.remove(num);
-					}
-					if(clientList.size()<1){PokerServer.deletePartie(this);}
-						available.release();
-		}catch(Exception e){e.printStackTrace();}
-													}
-	/*
-	Fonction pour ajouter un client à la partie
-	*/								
-	public int addPlayer(PokerClientThread np){
-	if(clientList.size()<maxPlayers){
-	clientList.add(np);	return 0;}
-	else return -1;
-											}
-				
+    private Vector clientList= new Vector();
+    private PrintWriter screenOut = new PrintWriter(System.out, true);
+    private int enCours = 0;
+    private int maxPlayers = 8;
+    private Semaphore available = new Semaphore(1, true);
+    private String nomP = "";
 
-				
-		/*
-		Fonction pour libérer la mémoire
-		*/				
-	protected void finalize() throws Throwable{
-	
-	 try {
-        clientList.clear();
-	available  =null;
-	nomP = null;
-      screenOut.close();
-       
-    } catch(Exception e) {e.printStackTrace();}
-    finally {
-		  super.finalize();
+    /**
+    *Constructeur
+    *@param nom : nom de la partie
+    * @param max : nombre de joueurs maximum dans la partie
+    * @author ben maurin
+    */
+    public PokerPartie(String nom,int max){
+        this.nomP = nom;
+        this.maxPlayers= max;
     }
-	 
-	}
-											
+	
+    /**
+     * 
+     * @return clientList
+     */
+    public Vector getClientList(){
+        return clientList;
+    }
+    
+    /**
+     * 
+     * @return nombre joueurs dans la partie
+     */
+    public String getEtat(){ 
+        return clientList.size()+"/"+maxPlayers+" joueurs dans la partie";
+    }
+    
+    /**
+     * 
+     * @param m maxPlayeur
+     */
+    public void setMaxPlayers(int m){
+        this.maxPlayers=m;
+    }
+    
+    /**
+     * 
+     * @return maxPlayers
+     */
+    public int getMaxPlayers(){
+        return this.maxPlayers;
+    }
+    
+    /**
+     * 
+     * @return nombre de joueurs dans la partie
+     */
+    public int getNbJ(){
+        return clientList.size();
+    }
+    
+    /**
+     * 
+     * @return nom de la partie
+     */
+    public String getNom(){
+        return this.nomP;
+    }
+
+
+    /**
+     * Fonction pour envoyer un message Ã  tous les clients de la partie
+     * @param m : message Ã  envoyer
+     */
+    public void broadcastClientsPartie(String m)	{
+        int i;
+        PokerClientThread foo;
+        for (i=0;i<clientList.size();++i){
+            foo = (PokerClientThread)clientList.get(i);
+            foo.send(m);
+        } 	
+    }
+    
+    /**
+     * Fonction pour enlever un client de la partie
+     * @param deadClient client Ã  supprimer
+     */
+    public void deleteClient(PokerClientThread deadClient){
+        try{
+            available.acquire();
+            int num;
+            num=clientList.indexOf(deadClient);
+            if(num!=-1){
+                clientList.remove(num);                         
+            }                       
+            if(clientList.size()<1){
+                PokerServer.deletePartie(this);
+            }
+            available.release();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+
+    /**
+     * Fonction pour ajouter un client Ã  la partie
+     * @param np nouveau player
+     * @return
+     */
+    public int addPlayer(PokerClientThread np){
+    if(clientList.size()<maxPlayers){
+    clientList.add(np);	return 0;}
+    else return -1;
+                                                                                    }
+
+    /**
+    * Fonction pour libÃ©rer la mÃ©moire
+    * @throws Throwable
+    */
+    @Override
+    protected void finalize() throws Throwable{
+        try {
+            clientList.clear();
+            available  =null;
+            nomP = null;
+            screenOut.close();
+        }catch(Exception e) {
+        
+        }
+        finally {
+            super.finalize();
+        } 
+    }											
 }
