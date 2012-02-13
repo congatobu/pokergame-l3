4da7183d7262e20a9113a8971013eb7739ecfdd6
Ben.Maurin@gmail.com <Ben.Maurin@gmail.com@42773be0-cb6b-67bc-c68f-7b7c866795fe>
2012-02-13 18:03:49 +0000

    Rajout commentaires classes (javadoc) tous fichiers serveur DONT JEU HEIN STEVE

--- trunk/serveur/PokerServer_début/PokerClientThread.java
+++ trunk/serveur/PokerServer_début/PokerClientThread.java
@@ -1,174 +1,172 @@
 
-import java.net.*;
+
 import java.io.*;
-import java.util.*;
+import java.net.Socket;
 
-
-
-////  CLASSE THREAD  (chaque client a un thread)---------------------------------------------
-
-//-------------------------------------------------------------
+/**
+ * Classe du thread par client qui gère les messages avec son client
+ * @author ben Maurin
+ */
 class PokerClientThread extends Thread {
-	private PrintWriter screenOut = new PrintWriter(System.out, true);
-	private Socket socket = null;
-	private String clientIP;
-	private BufferedWriter out;
-	private BufferedReader in;
-	private PokerPartie partie= null;
-	private String pseudo = "";
+    private PrintWriter screenOut = new PrintWriter(System.out, true);
+    private Socket socket = null;
+    private String clientIP;
+    private BufferedWriter out;
+    private BufferedReader in;
+    private PokerPartie partie= null;
+    private String pseudo = "";
 
-	/*
-	Constructeur
-	*/
-	public PokerClientThread(Socket socket){
-		super("PokerClientThread");
-		this.socket = socket;
-	}
-
-		/*
-	Fonction générale reception messages
-	*/
-	public void run(){
-		clientIP = socket.getInetAddress().getHostAddress();
-				try{
-				//new PrintWriter(socket.getOutputStream(), true);
-			out =  new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-			in = new BufferedReader (new InputStreamReader(socket.getInputStream()));
-			String inputLine;
-			
-			
-				
-				
-				 try {
-				  // lance le traitement du message reçu
-   while ((inputLine = in.readLine()) != null) {
-   
-			traitements(inputLine);
-
-      }
-  } catch (Exception e) {
-  
-  screenOut.println("ya un bug dans un thread reception message "+e);
-  e.printStackTrace();
-        }
- 
-				
-			deco();
-			
-		
-		}catch(IOException e){
-		screenOut.println("ya un bug dans un thread (bug général)");
-		e.printStackTrace();
-			deco();
-		}	
-			
-	}
-	
-		/*
-	Fonction pour traitements messages
-	*/
-	private void traitements(String inputLine){
-	 
-						try{
-						//perroquet test
-		screenOut.println("Socket recue : "+inputLine);
-		send(clientIP+" : "+inputLine);
-   	
-						//Reception nom des clients
-			if(inputLine.length()>=8 && inputLine.substring(0,8).equals("<pseudo>")){
-							this.pseudo = inputLine.substring(8);
-							screenOut.println("Pseudo enregistre : "+inputLine);
-								}
-								
-									//Reception messages des clients
-			if(inputLine.length()>=9 && inputLine.substring(0,9).equals("<message>")){
-							screenOut.println("Message recu : "+inputLine);
-								}	
-	
-						}
-						catch(Exception e){
-						 screenOut.println("ya un bug traitements message");
-						 e.printStackTrace();
-						 }
-	}
-	
-	
-	/*
-	accesseurs
-	*/
-	public String getClientIP(){ return this.clientIP;}
-	public String getPseudo(){ return this.pseudo;}
-	
-		/*
-	Fonction pour déconnecter proprement
-	*/
-	public void deco(){
-	try{
-	in.close();
-	out.close();
-	socket.close();	
-	if(partie!=null){partie.deleteClient(this);}
-	PokerServer.deleteClient(this);
-			}catch(IOException e){
-		screenOut.println("ya un bug dans un thread deco"+e);
-		e.printStackTrace();
-		}	
-			
-	}
-
-		/*
-		Fonction pour libérer la mémoire
-		*/				
-	protected void finalize() throws Throwable{
-	
-	 try {
-	 socket = null;
-	 clientIP=null;
-	 out = null;
-	 in = null;
-	 partie= null;
-	 pseudo = null;
-      screenOut.close();
-       
-    } catch(Exception e) {e.printStackTrace();}
-    finally {
-		  super.finalize();
+    public PokerClientThread(Socket socket){
+        super("PokerClientThread");
+        this.socket = socket;
+        System.err.println();
     }
-	 
-	}
+
+    /**
+     * Fonction qui receptionne les messages
+     */
+    @Override
+    public void run(){
+        clientIP = socket.getInetAddress().getHostAddress();
+        try{
+            //new PrintWriter(socket.getOutputStream(), true);
+            out =  new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            in = new BufferedReader (new InputStreamReader(socket.getInputStream()));
+            String inputLine;
+
+            try {
+                // lance le traitement du message reçu
+                while ((inputLine = in.readLine()) != null) {
+                    traitements(inputLine);
+                }
+            } catch (Exception e) {
+                screenOut.println("ya un bug dans un thread reception message "+e);
+            }
+            deco();
+        }catch(IOException e){
+            screenOut.println("ya un bug dans un thread (bug général)");
+            deco();
+        }	
+    }
+
+    /**
+     * Fonction qui traite le message en fonction de ses balises
+     * @param inputLine : le message à traiter
+     */
+    private void traitements(String inputLine){
+        try{
+            //perroquet test
+            screenOut.println("Socket recue : "+inputLine);
+            send(clientIP+" : "+inputLine);
+
+            //Reception nom des clients
+            if(inputLine.length()>=8 && inputLine.substring(0,8).equals("<pseudo>")){
+                this.pseudo = inputLine.substring(8);
+                screenOut.println("Pseudo enregistre : "+inputLine);
+            }
+
+            //Reception messages des clients
+            if(inputLine.length()>=9 && inputLine.substring(0,9).equals("<message>")){
+                screenOut.println("Message recu : "+inputLine);
+            }	
+
+        }catch(Exception e){
+            screenOut.println("ya un bug traitements message");
+        }
+    }
+
+    /**
+     * 
+     * @return clientIP
+     */
+    public String getClientIP(){ 
+        return this.clientIP;
+    }
+    
+    /**
+     * 
+     * @return pseudo
+     */
+    public String getPseudo(){ 
+        return this.pseudo;
+    }
+
+    /**
+     * Fonction pour une deconnection propre
+     */
+    public void deco(){
+        try{
+            in.close();
+            out.close();
+            socket.close();	
+            if(partie!=null){
+                partie.deleteClient(this);
+            }
+            PokerServer.deleteClient(this);
+        }catch(IOException e){
+            screenOut.println("ya un bug dans un thread deco"+e);
+        }	
+    }
 	
-	/*
-	Fonction pour rejoindre une partie
-	*/
-	public void rejoindrePartie(String nomP)
-	{	int existe = 0;
-		PokerPartie foo = null;
-		for (int i=0;i<PokerServer.partiesList.size();++i){
-			foo = (PokerPartie)PokerServer.partiesList.get(i);
-			if(foo.getNom().equals(nomP)){existe=1;break;}
-		}
-		
-		if(existe==1){
-		if(foo.addPlayer(this)==-1){existe = 0;}else{partie=foo;}
-		}
-		if(existe==0){send("probleme partie trop de joueurs ou partie inconnue");
-			}
-	}
-	
-	
-	/*
-	Fonction pour envoyer un message au client
-	*/
-		public void send(String message){
-		try{
-		out.write(message+"\n");
-		out.flush();
-		 screenOut.println("Message envoye : "+message);
-		 }
-		 catch(Exception e){
-		 screenOut.println("ya un bug envoi message");
-		 e.printStackTrace();
-		 }
-		
-	}
+    /**
+     * Fonction pour optimiser la suppression de l'objet
+     * @throws Throwable 
+     */
+    @Override
+    protected void finalize() throws Throwable{
+        try {
+            socket = null;
+            clientIP=null;
+            out = null;
+            in = null;
+            partie= null;
+            pseudo = null;
+            screenOut.close();
+        } catch(Exception e) {
+        
+        }
+        finally {
+            super.finalize();
+        }
+    }
+
+    /**
+     * Tente de rejoindre une partie, sinon envoi un message d'erreur au client
+     * @param nomP : nom de la partie à rejoindre
+     */
+    public void rejoindrePartie(String nomP){	
+        int existe = 0;
+        PokerPartie foo = null;
+        for (int i=0;i<PokerServer.partiesList.size();++i){
+            foo = (PokerPartie)PokerServer.partiesList.get(i);
+            if(foo.getNom().equals(nomP)){
+                existe=1;break;
+            }
+        }
+
+        if(existe==1){
+            if(foo.addPlayer(this)==-1){
+                existe = 0;
+            }else{
+                partie=foo;
+            }
+        }
+        if(existe==0){
+            send("probleme partie trop de joueurs ou partie inconnue");
+        }
+    }
+
+    /**
+     * Envoi un message au client
+     * @param message : message à envoyer
+     */
+    public void send(String message){
+        try{
+            out.write(message+"\n");
+            out.flush();
+            screenOut.println("Message envoye : "+message);
+        }catch(Exception e){
+            screenOut.println("ya un bug envoi message");
+        }
+    }
 }
