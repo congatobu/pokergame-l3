1caee00933509d6daec49f6a7c41bdb8b502ac78
Ben.Maurin@gmail.com <Ben.Maurin@gmail.com@42773be0-cb6b-67bc-c68f-7b7c866795fe>
2012-02-14 19:00:18 +0000

    Suppression Jeu + dossier BDD + plein de changements serveurs

--- trunk/serveur/PokerServer_début/src/PokerClientThread.java
+++ trunk/serveur/PokerServer_début/src/PokerClientThread.java
@@ -5,7 +5,7 @@ import java.net.Socket;
 
 /**
  * Classe du thread par client qui gère les messages avec son client
- * @author ben Maurin
+ * @author Benjamin Maurin
  */
 class PokerClientThread extends Thread {
     private PrintWriter screenOut = new PrintWriter(System.out, true);
@@ -15,6 +15,8 @@ class PokerClientThread extends Thread {
     private BufferedReader in;
     private PokerPartie partie= null;
     private String pseudo = "";
+    private ClientBDD bd;
+    
 
     public PokerClientThread(Socket socket){
         super("PokerClientThread");
@@ -24,6 +26,7 @@ class PokerClientThread extends Thread {
 
     /**
      * Fonction qui receptionne les messages
+     * @author Benjamin Maurin
      */
     @Override
     public void run(){
@@ -49,34 +52,10 @@ class PokerClientThread extends Thread {
         }	
     }
 
-    /**
-     * Fonction qui traite le message en fonction de ses balises
-     * @param inputLine : le message à traiter
-     */
-    private void traitements(String inputLine){
-        try{
-            //perroquet test
-            screenOut.println("Socket recue : "+inputLine);
-            send(clientIP+" : "+inputLine);
 
-            //Reception nom des clients
-            if(inputLine.length()>=8 && inputLine.substring(0,8).equals("<pseudo>")){
-                this.pseudo = inputLine.substring(8);
-                screenOut.println("Pseudo enregistre : "+inputLine);
-            }
-
-            //Reception messages des clients
-            if(inputLine.length()>=9 && inputLine.substring(0,9).equals("<message>")){
-                screenOut.println("Message recu : "+inputLine);
-            }	
-
-        }catch(Exception e){
-            screenOut.println("ya un bug traitements message");
-        }
-    }
 
     /**
-     * 
+     * @author benjamin Maurin
      * @return clientIP
      */
     public String getClientIP(){ 
@@ -84,7 +63,7 @@ class PokerClientThread extends Thread {
     }
     
     /**
-     * 
+     * @author benjamin Maurin
      * @return pseudo
      */
     public String getPseudo(){ 
@@ -93,6 +72,7 @@ class PokerClientThread extends Thread {
 
     /**
      * Fonction pour une deconnection propre
+     * @author benjamin Maurin
      */
     public void deco(){
         try{
@@ -110,6 +90,7 @@ class PokerClientThread extends Thread {
 	
     /**
      * Fonction pour optimiser la suppression de l'objet
+     * @author benjamin Maurin
      * @throws Throwable 
      */
     @Override
@@ -121,6 +102,7 @@ class PokerClientThread extends Thread {
             in = null;
             partie= null;
             pseudo = null;
+            bd = null;
             screenOut.close();
         } catch(Exception e) {
         
@@ -132,6 +114,7 @@ class PokerClientThread extends Thread {
 
     /**
      * Tente de rejoindre une partie, sinon envoi un message d'erreur au client
+     * @author benjamin Maurin
      * @param nomP : nom de la partie à rejoindre
      */
     public void rejoindrePartie(String nomP){	
@@ -158,6 +141,7 @@ class PokerClientThread extends Thread {
 
     /**
      * Envoi un message au client
+     * @author Benjamin Maurin
      * @param message : message à envoyer
      */
     public void send(String message){
@@ -169,4 +153,64 @@ class PokerClientThread extends Thread {
             screenOut.println("ya un bug envoi message");
         }
     }
+    
+    /**
+     * 
+     * Traite la base de données
+     * @author benjamin Maurin
+     */
+    public void traiteBD()
+    {
+        
+        /*
+           ClientBDD clientBDD = new ClientBDD();
+        clientBDD.Ouverture();
+        String[] temp;
+        
+        clientBDD.effaceBase();
+        
+        System.out.println(clientBDD.ajouteJoueur("Shyzkanza", "plop"));
+        System.out.println(clientBDD.verifPassword("Shyzkanza", "sniff"));
+        System.out.println(clientBDD.verifPassword("Shyzkanzo", "tsssss")+"\n");
+        
+        System.out.println(clientBDD.changePseudo("Shyzkanzi", "plop", "Shyzkanzu"));
+        System.out.println(clientBDD.changePseudo("Shyzkanza", "plop", "Shyzkanzo")+"\n");
+        System.out.println(clientBDD.changeMotDePasse("Shyzkanzo", "sniff", "sniff"));
+        System.out.println(clientBDD.changeMotDePasse("Shyzkanzo", "plop", "sniff")+"\n");
+        clientBDD.gagnePartie("Mathieu");
+        clientBDD.perdPartie("Mathieu");*/
+        
+    }
+    
+    
+    
+   
+    
+        /**
+     * Fonction qui traite le message en fonction de ses balises
+     * @author benjamin Maurin
+     * @param inputLine : le message à traiter
+     */
+    private void traitements(String inputLine){
+        try{
+            //perroquet test
+            screenOut.println("Socket recue : "+inputLine);
+            send(clientIP+" : "+inputLine);
+
+            //Reception nom des clients
+            if(inputLine.length()>=8 && inputLine.substring(0,8).equals("<pseudo>")){
+                this.pseudo = inputLine.substring(8);
+                screenOut.println("Pseudo enregistre : "+inputLine);
+            }
+
+            //Reception messages des clients
+            if(inputLine.length()>=9 && inputLine.substring(0,9).equals("<message>")){
+                screenOut.println("Message recu : "+inputLine);
+            }	
+
+        }catch(Exception e){
+            screenOut.println("ya un bug traitements message");
+        }
+    }
+    
 }
