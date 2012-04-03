d70741a91ad612be9be42e3fca621a5880b8a0b3
giner.steve <giner.steve@gmail.com>
2012-04-03 12:54:16 +0000

    ajout timer a la partie coté serveur

---
+++ serveur/PokerServer_début/src/TimerChoixJoueur.java
@@ -0,0 +1,56 @@
+
+
+
+import java.util.concurrent.Semaphore;
+
+
+
+/**
+ * classe permettant de ne pas bloquer sur un choix de joueur
+ * passe au joueur suivant au bout de 10 sec
+ * @author Benjamin Maurin 
+ */
+public class TimerChoixJoueur extends Thread {
+ 
+    
+     private PokerPartie partie = null;
+     private PokerClientThread client = null;
+     private int machin = 1;
+       public TimerChoixJoueur(PokerPartie partie,PokerClientThread client){
+            this.partie = partie;
+            this.client=client;
+                }
+       
+       public void stopeux()
+       {
+    	   machin=0;
+       }
+       
+     public void run(){
+     {
+        try
+        {
+                Thread.sleep (15000); // En pause pour 15 secondes
+          if(machin==1){
+            if(partie!=null)
+            {
+                   String s=partie.jouage(1, 0, client);
+                   if(client!=null){
+                	   
+                	   client.send(s);
+                   }
+            }
+          }
+        }
+        catch (InterruptedException exception){exception.printStackTrace();}
+        }
+    }
+
+	
+     
+     
+     
+     
+}
+
+
